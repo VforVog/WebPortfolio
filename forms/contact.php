64fb4fb46af1040e4dcdee5a3678f7098d22442d
VforVog <stelios_vogiatzis@hotmail.com>
2025-04-24 19:20:23 +0300

    V1

--- forms/contact.php
+++ forms/contact.php
@@ -20,8 +20,8 @@
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
+    'host' => 'smtp-mail.outlook.com',
+    'username' => 'stelios_vogiatzis@hotmail.com',
     'password' => 'pass',
     'port' => '587'
   );
